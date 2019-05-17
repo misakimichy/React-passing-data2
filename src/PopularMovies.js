9f5652194e4c8acc93de063cee37b91ea5b5b844
Misaki Koonce <misaki.koonce@gmail.com>
2019-05-17 11:24:12 -0700

    Fixed intent

--- src/PopularMovies.js
+++ src/PopularMovies.js
@@ -5,17 +5,17 @@ import MovieCard from './MovieCard.js';
 class PopularMovies extends Component {
 	render() {
       //console.log('Prop',this.props);
-      const {userByMovie, users, movies} = this.props
+      const {userByMovie, users, movies} = this.props;
 
       const movieCards = Object.keys(movies).map(id => (
         <MovieCard key={id}
-        users={users}
-        userWhoLikedMovie={userByMovie[id]}
-        movieInfo={movies[id]}
+          users={users}
+          userWhoLikedMovie={userByMovie[id]}
+          movieInfo={movies[id]}
         />
-      ))
+      ));
       
-	  //Return JSX
+	    // Return JSX
       return <div>{movieCards}</div>;
     }
 }
