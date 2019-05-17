db1cdf09e62546ec31e6f081aa3d26fc47700976
Misaki Koonce <misaki.koonce@gmail.com>
2019-05-17 11:23:22 -0700

    Pass data to UserList component

--- src/MovieCard.js
+++ src/MovieCard.js
@@ -5,13 +5,16 @@ class MovieCard extends Component {
     render() {
         const {users, userWhoLikedMovie, movieInfo} = this.props;
 
+        // Return JSX
         return (
          <li key={movieInfo.id}>
            <h2>{movieInfo.name}</h2>
            <p>Liked By</p>
-            <UserList />
+            <UserList 
+                users={users}
+                userWhoLikedMovie={userWhoLikedMovie}
+            />
         </li>
-        
         );
     }
 }
